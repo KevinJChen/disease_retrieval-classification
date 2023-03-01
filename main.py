b82fba23f8a913e6db07fbac75fa2deeb8dc1cd8
KevinJChen <jiachen2k@gmail.com>
2023-03-01 14:46:22 -0800

    finished retrieval, working on ui

--- main.py
+++ main.py
@@ -8,32 +8,14 @@ import os
 '''
 patientID -> disease -> medication -> gene 
 
--> pathway -> pathway information
-
-
-
-
-patientID - disease - medication - 
-
-RxNorm
-
-
-disease -> medication -> gene -> gene information
-
-
-
 disease -> medication
-SYNTHEA data: retrieve medication from those prescribed due to disease
+SYNTHEA data: https://synthea.mitre.org
+- retrieve medication from those prescribed due to disease
 
-medication -> pathway
-
-PharmGKB: https://www.pharmgkb.org/pathways
-Drug Bank: https://go.drugbank.com
-
+medication/drug -> gene interaction
 DGIdb: https://www.dgidb.org/api
+- retrieve drug-gene interactions
 
-pathway -> pathway information
-REACTOME API: query with pathway/pathway ID
 '''
 
 
@@ -104,41 +86,114 @@ def main():
                     f.write(str(interactions) + ",")
                 f.write(str(dgi[2]) + "\n")
 
-
-    # combining everything into one hashmap (dictionary)
-    patient_information = {}
-    for k, v in id_medication.items():
-
-        # patient ID (key)
-        patient_information[k] = []
-
-        # disease (value[0])
-        patient_information[k].append(v[0])
-
-        # medication list (value[1])
-        patient_information[k].append(v[1:])
-
-        all_interactions = []
-        for interaction in drug_gene_interactions:
-            if interaction[0] in v[1:]:
-                all_interactions.append(interaction)
-
-        # drug-gene interactions (value[2])
-        patient_information[k].append(all_interactions)
-
-    for k, v in patient_information.items():
-        print("Here is the information for patient (" + str(k) + ")")
-        print("/tThey have the condition: " + str(v[0]))
-        for medication in v[1]:
-            for interaction in v[2]:
-                if medication == interaction[0]:
-                    print("The medication/drug, " + str(medication) + " is an " + str(interaction[1] + ".") +
-                          " with the gene, " + str(interaction[2]))
-
+    # throw everything into a text file
+    # patient ID, disease
+    # \t medication1, type1, gene1, type2, gene2, type3, gene3...
+    if os.path.exists('patient_information.txt'):
+        print("Information will be retrieved from patient_information.txt...")
+    else:
+        with open('patient_information.txt', 'w') as f:
+            for k, v in id_medication.items():
+                f.write(str(k) + "," + v[0] + "," + str(len(v[1:-1])+1) + "\n")
+                for medication in v[1:]:
+                    f.write(str(medication) + ",")
+                    for interaction in drug_gene_interactions:
+                        if medication == interaction[0]:
+                            # if no interaction type - influencer
+                            if not interaction[1:-1][0]:
+                                f.write("influencer,")
+                            else:
+                                f.write(str(interaction[1:-1][0][0]) + ",")
+                            f.write(str(interaction[-1]) + ",")
+                f.write("\n")
+        print("Patient information stored in patient_information.txt")
 
     # track time of program
     end = time.time()
     print("Program took : " + str(end - start) + " seconds to run")
+    print()
+
+    with open('output.txt', 'w') as f:
+        pass
+    print("All information is stored in patient_information.txt")
+    print()
+    print("What information would you like to query?")
+    print(" PATIENT | DISEASE | MEDICATION | GENE")
+    query_type = input()
+
+    if query_type.lower() == "patient":
+        print("Which patient would you like to receive information for? "
+              "If you would like to list all patient IDs, please type \"all.\"")
+        patient_input = input()
+        if patient_input.lower() == "all":
+            with open('output.txt', 'w') as f:
+                for patient_ID in id_medication.keys():
+                    f.write(str(patient_ID) + "\n")
+        elif patient_input.lower() in id_medication.keys():
+            to_write = []
+            with open('patient_information.txt', 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    tokenized = line.split(",")
+                    if tokenized[0] == patient_input.lower():
+                        to_write.append(str(tokenized[0]) + "," + str(tokenized[1]) + "\n")
+                        for i in range(0, int(tokenized[2])):
+                            to_write.append(lines[lines.index(line)+i+1])
+            with open('output.txt', 'w') as f:
+                for line in to_write:
+                    f.write(str(line))
+        else:
+            print("Invalid patient ID.")
+    elif query_type.lower() == "disease":
+        print("Which disease would you like to receive information for? "
+              "If you would like to list all diseases, please type \"all.\"")
+        disease_input = input()
+        if disease_input.lower() == "all":
+            with open('output.txt', 'w') as f:
+                disease_set = set()
+                for k, v in id_medication.items():
+                    disease_set.add(v[0])
+                for disease in disease_set:
+                    f.write(str(disease) + "\n")
+        else:
+            with open('output.txt', 'w') as f:
+                for interaction in drug_gene_interactions:
+
+
+
+        if os.path.getsize('output.txt') == 0:
+            print("Invalid disease name.")
+    elif query_type.lower() == "medication":
+        print("Which medication would you like to receive information for? "
+              "If you would like to list all medications, please type \"all.\"")
+        medication_input = input()
+        if medication_input.lower() == "all":
+            with open('output.txt', 'w') as f:
+                medication_set = set()
+                for k, v in id_medication.items():
+                    for medication in v[1:]:
+                        medication_set.add(medication)
+                for medication in medication_set:
+                    f.write(str(medication) + "\n")
+        if os.path.getsize('output.txt') == 0:
+            print("Invalid medication name.")
+    elif query_type.lower() == "gene":
+        print("Which gene would you like to receive information for? "
+              "If you would like to list all genes, please type \"all.\"")
+        gene_input = input()
+        if gene_input.lower() == "all":
+            with open('output.txt', 'w') as f:
+                gene_set = set()
+                for interaction in drug_gene_interactions:
+                    gene_set.add(interaction[2])
+                for gene in gene_set:
+                    f.write(str(gene) + "\n")
+        if os.path.getsize('output.txt') == 0:
+            print("Invalid gene name.")
+    else:
+        print("Unknown input.")
+        return
+    print("Query retrieved and results placed in output.txt")
 
 if __name__ == "__main__":
     main()
